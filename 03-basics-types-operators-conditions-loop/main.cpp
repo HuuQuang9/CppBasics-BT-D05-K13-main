008d4ade4a9c177610472c2d8cef0cf4305677c4
HuuQuang9 <quang91120@gmail.com>
2021-10-21 09:47:02 +0700

    sua btvn

--- 03-basics-types-operators-conditions-loop/main.cpp
+++ 03-basics-types-operators-conditions-loop/main.cpp
@@ -64,27 +64,8 @@ int main()
 		cout << "class: " << lop[j] << endl;
 		cout << "xep loai: " << xeploai[j] << endl;
 		cout << "so thich: " << sothich[j] << '\n';
-
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
 }
-
-
-
 
 // nhap vao nhieu sv
 // // Nhap vao sv: ho ten, tuoi, lop
@@ -93,14 +74,3 @@ int main()
 // Tinh diem TB cac mon hoc, xep loai
 // >8 gioi; 8 > TB >= 6.5 Kha; 6.5 > TB >=5 : TB; 
 // In ra thong tin sv
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
